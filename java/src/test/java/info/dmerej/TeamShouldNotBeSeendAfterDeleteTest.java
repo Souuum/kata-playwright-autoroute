5b30aaa539b78f22ab85c93f2c9e9db7a471a66e
Thomas Song <thomas.song@efrei.net>
2025-02-24 12:00:02 +0100

    Step 5 : Begin Refactor

--- java/src/test/java/info/dmerej/TeamShouldNotBeSeendAfterDeleteTest.java
+++ java/src/test/java/info/dmerej/TeamShouldNotBeSeendAfterDeleteTest.java
@@ -6,50 +6,36 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import info.dmerej.SearchPage;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TeamShouldNotBeSeendAfterDeleteTest {
-    private Page page;
+    private SearchPage searchPage;
+
+    public TeamShouldNotBeSeendAfterDeleteTest() {
+        this.searchPage = new SearchPage();
+    }
+
     @BeforeEach
     void setUp() {
-        var playwright = Playwright.create();
-        var launchOptions = new BrowserType.LaunchOptions().setHeadless(false)
-                .setSlowMo(1000); // Remove this when you're done debugging
-        var browser = playwright.chromium().launch(launchOptions);
-
-        // Set base URL for the new context
-        var contextOptions = new Browser.NewContextOptions();
-        // TODO: fix the URL
-        contextOptions.setBaseURL("https://a.lsi2.hr.dmerej.info");
-        var context = browser.newContext(contextOptions);
-
-        this.page = context.newPage();
-
-        // Reset database
-        page.navigate("/reset_db");
-        var proceedButton = page.locator("button:has-text('proceed')");
-        proceedButton.click();
-        page.navigate("/");
+        this.searchPage.setUp();
     }
+
     @Test
     void TeamShouldNotBeSeenAfterDelete() {
 
         // Add a new team
-        this.page.navigate("/add_team");
-        var nameInput = this.page.locator("input[name=\"name\"]");
         var teamName = "my team";
-        nameInput.fill(teamName);
-        this.page.click("text='Add'");
+        this.searchPage.navigateToAddTeam();
+        this.searchPage.addTeam(teamName);
 
-        // Check that the team has been added
-        this.page.navigate("/teams");
-
-        this.page.click("text='Delete'");
-        this.page.click("text='Proceed'");
+        // Navigate to teams and delete the team
+        this.searchPage.navigateToTeams();
+        this.searchPage.deleteTeam();
 
         // Go back
-        this.page.goBack();
+        this.searchPage.goBack();
 
         // Check the new team is not there
         String selector = String.format("td:has-text('%s')", teamName);
