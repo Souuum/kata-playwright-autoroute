68d0d054ce359593df94df2d31c1d597a75d5c69
Sophie Minos <sophie.minos@efrei.net>
2025-02-26 08:46:19 +0100

    refactor test PostalCodeShouldNotBeNegativeTest

--- java/src/test/java/info/dmerej/PostalCodeShouldNotBeNegativeTest.java
+++ java/src/test/java/info/dmerej/PostalCodeShouldNotBeNegativeTest.java
@@ -6,40 +6,18 @@ import org.junit.jupiter.api.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 public class PostalCodeShouldNotBeNegativeTest {
-    private Page page;
+
+    private AddEmployeePage addEmployeePage;
+
+    public PostalCodeShouldNotBeNegativeTest() { this.addEmployeePage = new AddEmployeePage(); }
     @BeforeEach
     void setUp() {
-        var playwright = Playwright.create();
-        var launchOptions = new BrowserType.LaunchOptions().setHeadless(false)
-                .setSlowMo(1000); // Remove this when you're done debugging
-        var browser = playwright.chromium().launch(launchOptions);
-
-        // Set base URL for the new context
-        var contextOptions = new Browser.NewContextOptions();
-        // TODO: fix the URL
-        contextOptions.setBaseURL("https://a.lsi2.hr.dmerej.info");
-        var context = browser.newContext(contextOptions);
-
-        this.page = context.newPage();
-
-        // Reset database
-        page.navigate("/reset_db");
-        var proceedButton = page.locator("button:has-text('proceed')");
-        proceedButton.click();
-        page.navigate("/");
+        this.addEmployeePage.setUp();
     }
 
     @Test
     void PostalCodeShouldNotBeNegative() {
-        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add new employee")).click();
-        page.getByPlaceholder("Name").fill("TestName");
-        page.getByPlaceholder("Email").fill("test.name@gmail.com");
-        page.locator("#id_address_line1").fill("34 rue poirreau");
-        page.getByPlaceholder("City").fill("Paris");
-        page.getByPlaceholder("Zip code").fill("-1");
-        page.getByPlaceholder("Hiring date").fill("2025-02-27");
-        page.getByPlaceholder("Job title").fill("dev");
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add")).click();
-        assertThat(page.locator("tbody")).isEmpty();
+        this.addEmployeePage.addEmployee("TestName", "test.name@gmail.com", "34 rue poirreau", "Paris", "-1", "2025-02-27", "dev");
+        assertThat(this.page.locator("tbody")).isEmpty();
     }
 }
